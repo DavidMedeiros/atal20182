7312dd63b16d8326d3a4bf32208684e6f53c76cf
David Souza <david.souza@ccc.ufcg.edu.br>
2018-10-09 11:10:15 -0300

    :beetle: Improving sort algorith

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -1,8 +1,25 @@
 # Esse metodo recebe uma lista com as matriculas dos alunos
-# e retorna essa lista em ordem crescente de matriculas
+# e retorna essa lista em ordem DECRESCENTE de matriculas
 def retorna_matriculas_decrescente(alist):
-	for j in xrange(len(alist)):
-		for i in xrange(len(alist)-1):
-			if alist[i] > alist[i+1]:
-				alist[i], alist[i+1] = alist[i+1], alist[i]
+	quick(alist, 0, len(alist) -1)
 	return alist
+
+def quick(lista, inicio, fim):
+	if inicio < fim:
+		pivot = partition(lista, inicio, fim)
+
+		quick(lista, inicio, pivot - 1)
+		quick(lista, pivot + 1, fim)
+
+def partition(lista, inicio, fim):
+	pivot = lista[inicio]
+	index = inicio
+
+	for i in range(inicio + 1, fim + 1):
+		if lista[i] > pivot:
+			index += 1
+			lista[index], lista[i] = lista[i], lista[index]
+
+	lista[inicio], lista[index] = lista[index], lista[inicio]
+
+	return index
