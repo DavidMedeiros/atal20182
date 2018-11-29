27a7f757f004764357d91a9b1c8eb97784e7c21a
David Souza <david.souza@ccc.ufcg.edu.br>
2018-11-29 18:19:08 -0300

    Adicionando problema da mochila com PD

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -1,53 +1,18 @@
 #coding: utf-8
 
 import sys
-
-# Esse metodo recebe uma lista com as matriculas dos alunos
-# e retorna essa lista em ordem crescente de matriculas
-def retorna_matriculas_decrescente(alist):
-	for j in xrange(len(alist)):
-		for i in xrange(len(alist)-1):
-			if alist[i] > alist[i+1]:
-				alist[i], alist[i+1] = alist[i+1], alist[i]
-	return alist
-
-# Esse metodo recebe e valor para ser dado o troco e uma lista com os tipos de moedas possiveis
-# e retorna o numero minimo de moedas possiveis em que o troco pode ser dado
-#
-# Caso o valor não possa ser alcançado pela combinação de moedas o valor -1 é retornado Ex: valor = 11  moedas = {5, 10, 25}
-# Assuma que existe uma quantidade infinita de cada tipo de moeda
-def retorna_minimo_moedas(valor, tipos_moedas):
-	print valor, tipos_moedas
-	resultado = retorna_minimo_moedas_FB(tipos_moedas, valor)
-	if resultado == sys.maxint:
-		return -1
-	else:
-		return resultado
-
-def retorna_minimo_moedas_FB(tipos_moedas, valor):
-	if valor == 0:
-		return 0
-	
-	resultado = sys.maxint
-	
-	for moeda in tipos_moedas:
-		
-		if (moeda <= valor):
-			resultado = min(resultado, retorna_minimo_moedas_FB(tipos_moedas, valor - moeda) + 1) 
-		
-	return resultado	 
-
 # Mochila Binaria interativo
-  
-# Retorna o valor maximo que cabe na mochila com 
+
+# Retorna o valor maximo que cabe na mochila com
 # capacidade peso_maximo
-def mochila_binaria(peso_maximo, pesos, valores, n): 
-  
-    if n == 0 or peso_maximo == 0 : 
-        return 0
-  
-    if (pesos[n-1] > peso_maximo): 
-        return mochila_binaria(peso_maximo, pesos, valores, n-1) 
-    else: 
-        return max(valores[n-1] + mochila_binaria(peso_maximo-pesos[n-1], pesos, valores, n-1), 
-                   mochila_binaria(peso_maximo, pesos, valores, n-1)) 
+def mochila_binaria(peso_maximo, pesos, valores, n):
+    v = [[0] * (peso_maximo + 1) for _ in range(n + 1)]
+
+    for i in range(1, len(v)):
+        for j in range(1, len(v[i])):
+            if pesos[i-1] > j:
+                v[i][j] = v[i-1][j]
+            else:
+                v[i][j] = max(v[i-1][j - pesos[i-1]] + valores[i-1], v[i-1][j])
+
+    return v[n][peso_maximo]
